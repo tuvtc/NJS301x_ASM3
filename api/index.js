47a9b86dca9301c10a0b557c2a13687e7519bf59
tuvtcfx10680@funix.edu.vn <tuvtcfx10680@funix.edu.vn>
2023-03-14 23:09:43 +0700

    fix package.json

--- api/index.js
+++ api/index.js
@@ -17,6 +17,8 @@ app.use('/api/users', usersRouter)
 app.use('/api/products', productsRouter)
 app.use('/api/orders', authGuardCheck, ordersRouter)
 
+// Khi ko deploy thi xoa comment line phia duoi de chay local
 // app.listen(() => {console.log('Server is running in Vercel')})
 
+// export de chay duoi dang severless tren Vercel khi deploy
 module.exports = app;
