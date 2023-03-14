c6952c5e678963e3e8b1443ad660bb8aadc9034e
tuvtcfx10680@funix.edu.vn <tuvtcfx10680@funix.edu.vn>
2023-03-14 21:25:49 +0700

    fix

---
+++ api/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const cors = require('cors')
+const connectToDb = require('../db/database')
+const usersRouter = require('../routes/users')
+const productsRouter = require('../routes/products')
+const ordersRouter = require('../routes/orders')
+const authGuardCheck = require('../middlewares/auth')
+
+connectToDb()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use('/users', usersRouter)
+app.use('/products', productsRouter)
+app.use('/orders', authGuardCheck, ordersRouter)
+
+// app.listen(() => {console.log('Server is running in Vercel')})
+
+module.exports = app;
